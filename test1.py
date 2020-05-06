780c582f09f15c87f21a218c893f08f454a5e6b3
malimin <1052163211@qq.com>
2020-05-06 17:03:25 +0800

    2020年5月6日17点03分：test

--- test1.py
+++ test1.py
@@ -1,3 +1,6 @@
 # from test_transf_file-4-new import *
 
 from test2 import *
+
+
+printf()
---
+++ test2.py
@@ -0,0 +1,9 @@
+#-*- coding:utf-8 -*-
+# Create by MaLimin
+# Create on 2020/5/6
+
+
+def printf():
+    print(2)
+    return 0
+    print(222)
